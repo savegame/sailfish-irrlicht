b7b0aa501f790452f3c204ef860473a345145973
savegame <savegame@github.com>
2017-08-18 18:07:32 +0300

    * adding Wayland device listening (keyboard, touch, mouse)

--- source/Irrlicht/CIrrDeviceLinux.h
+++ source/Irrlicht/CIrrDeviceLinux.h
@@ -411,10 +411,17 @@ namespace irr
 		#endif
 #elif defined(SAILFISH)
 	public:
-		const struct wl_registry_listener listener;
+		const  struct wl_registry_listener wlListener;
 		static struct wl_compositor *wlCompositor;
 		static struct wl_shell *wlShell;
 		static struct wl_shell_surface_listener shell_surface_listener;
+		static struct wl_seat  *wlSeat; // Group of Wayland input devices (mice, keyboard and touch)
+
+
+//		void  wl_callback_done(void *data, struct wl_callback *wl_callback, uint32_t callback_data);
+//	protected:
+//		void global_registry_handler(void *data, struct wl_registry *registry, uint32_t id,
+//		                        const char *interface, uint32_t version);
 	private:
 		struct wl_surface *wlSurface;
 		struct wl_egl_window *wlEGLWindow;
@@ -422,24 +429,24 @@ namespace irr
 		struct wl_shell_surface *wlShellSurface;
 		struct wl_display * wlDisplay;
 		struct wl_registry *wlRegistry;
+		struct wl_callback *wlCallback;
 		/// Native System informations
 		NativeDisplayType nativeDisplay;
 		NativeWindowType nativeWindow;
-		//        uint16_t window_width, window_height;
 		/// EGL display
 		EGLDisplay  Display;
-//		/// EGL context
+		/// EGL context
 		EGLContext  Context;
-//		/// EGL surface
+		/// EGL surface
 		EGLSurface  Surface;
 #endif
-		u32 Width, Height;
+		u32  Width, Height;
 		bool WindowHasFocus;
 		bool WindowMinimized;
 		bool UseXVidMode;
 		bool UseXRandR;
 		bool ExternalWindow;
-		int AutorepeatSupport;
+		int  AutorepeatSupport;
 
 		struct SKeyMap
 		{
